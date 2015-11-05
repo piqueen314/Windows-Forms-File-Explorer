71f12dd350e40dae2da6e92343659c624c09dec2
Ceceilia Hedrick <cecehedrick@gmail.com>
2015-11-05 17:53:28 -0600

    add app first commit

---
+++ FileExplorer/mainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace FileExplorer
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directoryTreeView = new System.Windows.Forms.TreeView();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.viewRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // directoryTreeView
+            // 
+            this.directoryTreeView.Location = new System.Drawing.Point(12, 63);
+            this.directoryTreeView.Name = "directoryTreeView";
+            this.directoryTreeView.Size = new System.Drawing.Size(634, 161);
+            this.directoryTreeView.TabIndex = 0;
+            this.directoryTreeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.directoryTreeView_NodeMouseClick);
+            this.directoryTreeView.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.directoryTreeView_NodeMouseDoubleClick);
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(380, 37);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(75, 23);
+            this.enterButton.TabIndex = 1;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(12, 37);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(362, 20);
+            this.inputTextBox.TabIndex = 2;
+            this.inputTextBox.Text = "C:\\Users\\Ceceilia\\Documents";
+            // 
+            // viewRichTextBox
+            // 
+            this.viewRichTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.viewRichTextBox.Location = new System.Drawing.Point(12, 251);
+            this.viewRichTextBox.Name = "viewRichTextBox";
+            this.viewRichTextBox.ReadOnly = true;
+            this.viewRichTextBox.Size = new System.Drawing.Size(634, 217);
+            this.viewRichTextBox.TabIndex = 3;
+            this.viewRichTextBox.Text = "";
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(461, 37);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 23);
+            this.openButton.TabIndex = 4;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // SampleTextRead
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 499);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.viewRichTextBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.directoryTreeView);
+            this.Name = "SampleTextRead";
+            this.Text = "Sample Text Read";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView directoryTreeView;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.RichTextBox viewRichTextBox;
+        private System.Windows.Forms.Button openButton;
+    }
+}
+
---
+++ FileExplorer/mainForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileExplorer
+{
+    public partial class mainForm : Form
+    {
+        string substringDirectory; // store last part of full path name
+        private string substringFile;
+
+        public mainForm()
+        {
+            InitializeComponent();
+
+            inputTextBox.Text = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (Directory.Exists(inputTextBox.Text))
+            {
+                directoryTreeView.Nodes.Add(inputTextBox.Text);
+
+                PopulateTreeView(inputTextBox.Text, directoryTreeView.Nodes[0]);
+            }
+        }
+
+        public void PopulateTreeView(
+            string directoryValue, TreeNode parentNode)
+        {
+            string[] directoryArray =
+                Directory.GetDirectories(directoryValue);
+
+            try
+            {
+                string[] fileArray = Directory.GetFiles(directoryValue);
+                if (fileArray.Length != 0)
+                {
+                    foreach (string file in fileArray)
+                    {
+                        substringFile = Path.GetFileName(file);
+                        TreeNode myNode2 = new TreeNode(substringFile);
+                        parentNode.Nodes.Add(myNode2);
+                    }
+                }
+                if (directoryArray.Length != 0)
+                {
+                    foreach (string directory in directoryArray)
+                    {
+                        substringDirectory =
+                            Path.GetFileNameWithoutExtension(directory);
+                        TreeNode myNode = new TreeNode(substringDirectory);
+
+                        parentNode.Nodes.Add(myNode);
+
+                        PopulateTreeView(directory,myNode);
+                    }
+                }
+
+            }
+            catch (UnauthorizedAccessException)
+            {
+                parentNode.Nodes.Add("Acess denied");
+
+            }
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            directoryTreeView.Nodes.Clear();
+
+            if (Directory.Exists(inputTextBox.Text))
+            {
+                directoryTreeView.Nodes.Add(inputTextBox.Text);
+
+                PopulateTreeView(inputTextBox.Text,directoryTreeView.Nodes[0]);
+            }
+            else
+            {
+                MessageBox.Show(inputTextBox.Text + " could not be found.", 
+                    "Directory Not Found", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void directoryTreeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            inputTextBox.Text = e.Node.FullPath;
+        }
+
+        private void directoryTreeView_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+
+            if (File.Exists(inputTextBox.Text))
+            {
+                //add logic to read in directory contents 
+                string[] lines = File.ReadAllLines(inputTextBox.Text);
+                viewRichTextBox.Lines = lines;
+            }
+            else
+            {
+                if (Directory.Exists(inputTextBox.Text))
+                {
+                    directoryTreeView.Nodes.Add(inputTextBox.Text);
+
+                    PopulateTreeView(inputTextBox.Text, directoryTreeView.Nodes[0]);
+                }
+            }
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(inputTextBox.Text))
+            {
+               //add logic to read in directory contents 
+                string[] lines = System.IO.File.ReadAllLines(inputTextBox.Text);
+                viewRichTextBox.Lines = lines;
+            }
+            
+            else
+            {
+                MessageBox.Show(inputTextBox.Text + " could not be found.",
+                    "File Not Found", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
